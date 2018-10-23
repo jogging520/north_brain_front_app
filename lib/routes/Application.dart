aa31192d6beffc90ca706d9bbcb50eeb717eb6be
446734796@qq.com <wuqiang502>
2018-10-23 16:29:50 +0800

    增加了event_bus

--- lib/routes/Application.dart
+++ lib/routes/Application.dart
@@ -1,5 +1,10 @@
 
 import 'package:fluro/fluro.dart';
+import 'package:event_bus/event_bus.dart';
+
+///全局应用：包括路由、事件总线等。
+
+EventBus eventBus = new EventBus();
 
 class Application {
   static Router router;
